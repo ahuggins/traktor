daea51859b2ad5cb8880f6a6f7ba68206cc62690
Andy Huggins <andy@timetogowild.com>
2022-09-22 15:37:30 -0400

    add subcategory to data

--- src/commands/ParseSource.ts
+++ src/commands/ParseSource.ts
@@ -10,6 +10,7 @@ export default class ParseSource extends Command {
   static flags = {
     source: Flags.string(),
     file: Flags.string(),
+    url: Flags.string(),
   }
 
   public async run(): Promise<void> {
@@ -25,25 +26,33 @@ export default class ParseSource extends Command {
 
     const writer = createWriter(filename)
 
+    const parseUrlParams = new URLSearchParams(flags.url);
+
+    let subcategory: string | null = null;
+    if(parseUrlParams.get('last_category')) {
+      subcategory = parseUrlParams.get('last_category');
+    }
+
     $('.listing-wrapper').each((index:any, el: any) => {
 
       const $ = cheerio.load(el);
   
-      data.push({ 
-        auction_id_maybe: $('.col-xs-6:nth-child(3) .basic-bold a').attr('href').split('/').pop(),
-        title: $('h3').text(),
-        price: $('.auction-listing-price .listing-price:nth-child(1)').text(),
-        sold_date: $('.auction-listing-price .listing-price:nth-child(2) .basic-non-bold').text(),
-        specs_array: getFieldLabel('Specs:', $)?.split(', '),
-        specs: getFieldLabel('Specs:', $),
-        hours: getFieldLabel('Hours:', $),
-        condition: getFieldLabel('Condition:', $),
-        auction_event_details: $('.auction-event-details .auction-event-details').text()?.replace(' View on Map', ''),
-        auction_type: $('.col-xs-6:nth-child(3) .basic-bold .basic-non-bold').text()?.replaceAll('\n', ''),
-        auction_house_link: `https://www.machinerypete.com${$('.auction-event a').attr('href')}`,
-        auction_house_name: $('.auction-event a').text(),
-        year: parseInt($('h3').text().substring(0,4)) ? $('h3').text().substring(0,4) : 'unknown',
-    })  
+      data.push({
+          subcategory, 
+          auction_id_maybe: $('.col-xs-6:nth-child(3) .basic-bold a').attr('href').split('/').pop(),
+          title: $('h3').text(),
+          price: $('.auction-listing-price .listing-price:nth-child(1)').text(),
+          sold_date: $('.auction-listing-price .listing-price:nth-child(2) .basic-non-bold').text(),
+          specs_array: getFieldLabel('Specs:', $)?.split(', '),
+          specs: getFieldLabel('Specs:', $),
+          hours: getFieldLabel('Hours:', $),
+          condition: getFieldLabel('Condition:', $),
+          auction_event_details: $('.auction-event-details .auction-event-details').text()?.replace(' View on Map', ''),
+          auction_type: $('.col-xs-6:nth-child(3) .basic-bold .basic-non-bold').text()?.replaceAll('\n', ''),
+          auction_house_link: `https://www.machinerypete.com${$('.auction-event a').attr('href')}`,
+          auction_house_name: $('.auction-event a').text(),
+          year: parseInt($('h3').text().substring(0,4)) ? $('h3').text().substring(0,4) : 'unknown',
+      })  
     })
     if(data.length > 0) {
       await writer.writeRecords(data);
